37dcda55afaba612e81f09e29dc6e4a3b7f3cedf
DESKTOP-1NJKFD7\linn star <kkhakkha8@gmail.com>
2022-03-23 14:04:21 +0600

    list rendering

--- main.js
+++ main.js
@@ -11,7 +11,12 @@ const app = Vue.createApp({
             url : 'https://www.youtube.com',
             inStock: true,
             invetory : 100,
-            onSale : true
+            onSale : true,
+            details : ['50% cotton','30% wool', '20% polyester'],
+            variants : [
+                {id : 2234, color : 'green'},
+                {id : 2235, color : 'blue'}
+            ]
         }
     }
 })
