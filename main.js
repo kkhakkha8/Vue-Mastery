4925add6e876306e5c8ea873fa483306b623ee26
DESKTOP-1NJKFD7\linn star <kkhakkha8@gmail.com>
2022-03-24 00:44:43 +0600

    computed properties

--- main.js
+++ main.js
@@ -7,15 +7,14 @@ const app = Vue.createApp({
     data() {
         return{
             product : 'Socks',
-            image: './assets/images/socks_green.jpg',
+            brand : 'Vue Mastery',
             url : 'https://www.youtube.com',
-            inStock: false,
-            invetory :0,
+            selectedVariant : 0,
             onSale : true,
             details : ['50% cotton','30% wool', '20% polyester'],
             variants : [
-                {id : 2234, color : 'green', image: './assets/images/socks_green.jpg'},
-                {id : 2235, color : 'blue', image : './assets/images/socks_blue.jpg'}
+                {id : 2234, color : 'green', image: './assets/images/socks_green.jpg', quantity: 50},
+                {id : 2235, color : 'blue', image : './assets/images/socks_blue.jpg', quantity: 0}
             ],
             sizes : [33,34,35,40,42,44],
             cart : 0,
@@ -26,11 +25,23 @@ const app = Vue.createApp({
         addToCart() {
             this.cart += 1;
         },
-        updateImage(variantImage) {
-            this.image = variantImage;
+        updateVariant(index) {
+            this.selectedVariant = index;
+            
         },
         removeFromCart() {
             this.cart -= 1;
+        }
+    },
+    computed : {
+        title() {
+            return this.brand + ' ' + this.product;
+        },
+        image() {
+            return this.variants[this.selectedVariant].image;
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].quantity;
         }
     }
 })
