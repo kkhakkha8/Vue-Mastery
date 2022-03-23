c360578f8f58ea9daefc35f558eddd563769b8d2
DESKTOP-1NJKFD7\linn star <kkhakkha8@gmail.com>
2022-03-23 11:41:32 +0600

    attributes binding v-bind

---
+++ main.js
@@ -0,0 +1,14 @@
+const app = Vue.createApp({
+    methods: {
+        showDesc() {
+            alert('This is Description');
+        }
+    },
+    data() {
+        return{
+            product : 'Socks',
+            image: './assets/images/socks_green.jpg',
+            url : 'https://www.youtube.com'
+        }
+    }
+})
